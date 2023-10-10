1eeb39f98ee663d236583b955a05b37dd2e6d272
Karina Lage <karinar_lage@live.com>
2023-10-10 18:38:56 -0300

    first commit

--- inventory-tracker-frontend/src/app/abstract-entity.ts
+++
@@ -1,4 +0,0 @@
-export class AbstractEntity {
-    id: number;
-    name: string;
-}
--- inventory-tracker-frontend/src/app/user.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractEntity } from "./abstract-entity";
-
-export class User extends AbstractEntity {
-    username:string;
-    email:string;
-    phoneNumber:string;
-    pwHash:string;
-}
